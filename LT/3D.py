64980a2c6d939bab78ff1149e45b32a93e510c0e
KishorePrashanthP <88100231+KishorePrashanthP@users.noreply.github.com>
2024-05-28 18:50:08 +0530

    Add files via upload

---
+++ LT/3D.py
@@ -0,0 +1,48 @@
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+def cuboid_data2(o, size=(1,1,1)):
+    X = [[[0, 1, 0], [0, 0, 0], [1, 0, 0], [1, 1, 0]],
+         [[0, 0, 0], [0, 0, 1], [1, 0, 1], [1, 0, 0]],
+         [[1, 0, 1], [1, 0, 0], [1, 1, 0], [1, 1, 1]],
+         [[0, 0, 1], [0, 0, 0], [0, 1, 0], [0, 1, 1]],
+         [[0, 1, 0], [0, 1, 1], [1, 1, 1], [1, 1, 0]],
+         [[0, 1, 1], [0, 0, 1], [1, 0, 1], [1, 1, 1]]]
+    X = np.array(X).astype(float)
+    for i in range(3):
+        X[:,:,i] *= size[i]
+    X += np.array(o)
+    return X
+
+def plotCubeAt2(positions,sizes=None,colors=None, **kwargs):
+    if not isinstance(colors,(list,np.ndarray)): colors=["C0"]*len(positions)
+    if not isinstance(sizes,(list,np.ndarray)): sizes=[(1,1,1)]*len(positions)
+    g = []
+    for p,s,c in zip(positions,sizes,colors):
+        g.append( cuboid_data2(p, size=s) )
+    return Poly3DCollection(np.concatenate(g),  
+                            facecolors=np.repeat(colors,6),alpha=1.0, **kwargs)
+    
+
+with open('3Dpoints.json', 'r') as json_file:
+    loaded_data = json.load(json_file)
+
+positions = loaded_data['positions']
+sizes = loaded_data['sizes']
+colors = loaded_data['colors']
+
+fig = plt.figure(figsize=(7, 7))
+ax = fig.add_subplot(projection = '3d')
+ax.set_aspect('equal')
+
+pc = plotCubeAt2(positions,sizes,colors=colors, edgecolor="k")
+ax.add_collection3d(pc)    
+
+ax.set_xlim([0,255])
+ax.set_ylim([0,255])
+ax.set_zlim([0,255])
+
+plt.show()
